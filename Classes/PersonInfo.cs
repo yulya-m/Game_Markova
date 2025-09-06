4e373484db17c99502e2e623f0c81a8c6d2611ad
Markova Yulya <markovayulya0@yandex.ru>
2025-09-06 10:50:11 +0500

    Разработанный класс. Маркова

---
+++ Classes/PersonInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Markova.Classes
+{
+    public class PersonInfo
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Armor { get; set; }
+        public int Level { get; set; }
+        public int Glasses { get; set; }
+        public int Money { get; set; }
+        public float Damage { get; set; }
+
+
+    }
+}
